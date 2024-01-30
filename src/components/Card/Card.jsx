df6f50b2837e286da62322d54058ac4457c0d9a9
davialves2009 <157602051+davialves2009@users.noreply.github.com>
2024-01-30 13:37:22 +0000

    manda pro git hub

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,22 @@
+function Card(props){
+  //console.log(props)
+    const item = props.item
+
+    //console.log(item)
+       return (
+          <>
+            <div className='card'>
+              <h2>{item.name}</h2>
+                 <div className="tags">
+                  <div className="tag">Status: Vivo</div>
+                  <div className="tag">Espécie: Humana</div>
+                  <div className="tag">Origem: Terra c-137</div>
+                 </div>
+                <img src={item.image} />
+            </div>
+              
+            </>
+        )
+      }
+     
+      export default Card
